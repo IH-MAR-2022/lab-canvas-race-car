f28147342a11be8820ff12f5ceed2e0a32153b69
miketroianello <mike@Mikes-MacBook-Pro.local>
2022-03-31 16:08:49 -0400

    iteration 3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,59 @@
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
+  document.getElementById("start-button").onclick = () => {
     startGame();
   };
 
-  function startGame() {}
+  const player = {
+    x: 250,
+    y: 600,
+    w: 40,
+    h: 80,
+  };
+
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 500;
+  canvas.height = 700;
+  const h = canvas.height;
+  const w = canvas.width;
+
+  const car = new Image();
+  car.src = "images/car.png";
+  car.onload = function () {
+    ctx.drawImage(car, player.x, player.y, player.w, player.h);
+  };
+
+  function startGame() {
+    console.log("START");
+    animate();
+  }
+
+  function animate() {
+    console.log("Animating");
+    window.requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.drawImage(car, player.x, player.y, player.w, player.h);
+  }
+
+  function move(e) {
+    switch (e.key) {
+      case "ArrowLeft":
+        if (player.x - 10 < 0) {
+          player.x = 0;
+        } else {
+          player.x -= 10;
+        }
+        break;
+      case "ArrowRight":
+        if (player.x + player.w + 10 > w) {
+          player.x = w - player.w;
+        } else {
+          player.x += 10;
+        }
+        break;
+    }
+  }
+
+  document.addEventListener("keydown", move);
 };
