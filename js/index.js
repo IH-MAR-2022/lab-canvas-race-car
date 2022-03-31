524362d3923311a5843ec217867625e1a91ea25f
miketroianello <mike@Mikes-MacBook-Pro.local>
2022-03-31 16:51:28 -0400

    iteration 4

--- js/index.js
+++ js/index.js
@@ -10,6 +10,13 @@ window.onload = () => {
     h: 80,
   };
 
+  const obstacle = {
+    x: 200,
+    y: 0,
+    w: 140,
+    h: 20,
+  };
+
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
   canvas.width = 500;
@@ -28,12 +35,23 @@ window.onload = () => {
     animate();
   }
 
-  function animate() {
-    console.log("Animating");
-    window.requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "maroon";
 
+  let game;
+
+  function animate() {
+    game = window.requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(car, player.x, player.y, player.w, player.h);
+    //draw obstacle
+    ctx.fillRect(obstacle.x, obstacle.y, obstacle.w, obstacle.h);
+    obstacle.y++;
+    //this will evaluate to true or false
+    let didCollide = detectCollision(player, obstacle);
+    if (didCollide) {
+      console.log("You Died");
+      window.cancelAnimationFrame(game);
+    }
   }
 
   function move(e) {
@@ -56,4 +74,17 @@ window.onload = () => {
   }
 
   document.addEventListener("keydown", move);
+
+  function detectCollision(player, obj) {
+    if (
+      player.x < obj.x + obj.w &&
+      player.x + player.w > obj.x &&
+      player.y < obj.y + obj.h &&
+      player.y + player.h > obj.y
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
