4685299a9d0534bdea1d1581a061cdf7faa141d7
miketroianello <mike@Mikes-MacBook-Pro.local>
2022-03-31 17:13:10 -0400

    iteration 5

--- js/index.js
+++ js/index.js
@@ -17,6 +17,22 @@ window.onload = () => {
     h: 20,
   };
 
+  class Obstacle {
+    constructor() {
+      this.x = Math.random() * 400;
+      this.y = 0;
+      this.w = Math.random() * 300;
+      this.h = 20;
+    }
+  }
+
+  let obstacleArr = [];
+
+  function addObstacle() {
+    obstacleArr.push(new Obstacle());
+    console.log(obstacleArr);
+  }
+
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
   canvas.width = 500;
@@ -30,8 +46,11 @@ window.onload = () => {
     ctx.drawImage(car, player.x, player.y, player.w, player.h);
   };
 
+  let int;
+
   function startGame() {
     console.log("START");
+    int = setInterval(addObstacle, 2000);
     animate();
   }
 
@@ -44,14 +63,27 @@ window.onload = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(car, player.x, player.y, player.w, player.h);
     //draw obstacle
-    ctx.fillRect(obstacle.x, obstacle.y, obstacle.w, obstacle.h);
-    obstacle.y++;
-    //this will evaluate to true or false
-    let didCollide = detectCollision(player, obstacle);
-    if (didCollide) {
-      console.log("You Died");
-      window.cancelAnimationFrame(game);
+
+    for (let i = 0; i < obstacleArr.length; i++) {
+      ctx.fillRect(
+        obstacleArr[i].x,
+        obstacleArr[i].y,
+        obstacleArr[i].w,
+        obstacleArr[i].h
+      );
+      obstacleArr[i].y += 10;
+      //this will evaluate to true or false
+      let didCollide = detectCollision(player, obstacleArr[i]);
+      if (didCollide) {
+        gameOver();
+      }
     }
+  }
+
+  function gameOver() {
+    console.log("You Died");
+    window.cancelAnimationFrame(game);
+    clearInterval(int);
   }
 
   function move(e) {
@@ -60,14 +92,14 @@ window.onload = () => {
         if (player.x - 10 < 0) {
           player.x = 0;
         } else {
-          player.x -= 10;
+          player.x -= 15;
         }
         break;
       case "ArrowRight":
         if (player.x + player.w + 10 > w) {
           player.x = w - player.w;
         } else {
-          player.x += 10;
+          player.x += 15;
         }
         break;
     }
